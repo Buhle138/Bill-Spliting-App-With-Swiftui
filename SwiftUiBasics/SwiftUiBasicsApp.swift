c9e20e5796c539f0b5a516b351cee29b07f841cf
buhleradzilani <radzilanibuhle@gmail.com>
2024-05-19 09:44:12 +0200

    Initial Commit

---
+++ SwiftUiBasics/SwiftUiBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiBasicsApp.swift
+//  SwiftUiBasics
+//
+//  Created by Buhle Radzilani on 2024/05/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
