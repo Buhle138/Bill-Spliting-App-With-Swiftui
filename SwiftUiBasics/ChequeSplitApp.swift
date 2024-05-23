aeea62fcc5886e02f5fe198e0ba099a7bfefcc83
buhleradzilani <radzilanibuhle@gmail.com>
2024-05-23 12:23:22 +0200

    Adding the toolbar for the keyboard where the user clicks on done to dismiss the keyboard

--- SwiftUiBasics/ChequeSplitApp.swift
+++ SwiftUiBasics/ChequeSplitApp.swift
@@ -12,10 +12,21 @@ import SwiftUI
 struct ChequeSplitApp: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
-    @State private var tipPerentage = 20
+    @State private var tipPerentage = 20 //keeps track of the current tip that is chosen
+    @FocusState private var amountIsFocused: Bool
     
-    
-    let tipPercentage = [10, 15, 20, 25, 0]
+    let tipPercentage = [10, 15, 20, 25, 0] //stores the tip percentages to choose from.
+    var totalPerPerson: Double{
+        //calculate total per person here
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPerentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal =  checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     
     //when looping through an  array using a for each you must always use a  unique identifier (id)
     
@@ -26,6 +37,7 @@ struct ChequeSplitApp: View {
                 Section{
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     Picker("Number of people", selection: $numberOfPeople){
                         ForEach(2..<100){
                            Text("\($0) people")
@@ -50,10 +62,17 @@ struct ChequeSplitApp: View {
                 
                 
                 Section{
-                    Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                 }
             }
             .navigationTitle("weSplit")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard){
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
         
     }
